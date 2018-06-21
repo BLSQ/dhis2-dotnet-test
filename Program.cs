2be046803c7a6914400ef7025ac97387f09853be
Martin Van Aken <martin@joyouscoding.com>
2018-06-21 17:59:11 +0200

    First test

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+
+namespace marocco
+{
+    class Program
+    {
+        static HttpClient client = new HttpClient();
+        static void Main(string[] args)
+        {
+            var byteArray = Encoding.ASCII.GetBytes("admin:district");
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+
+            var response = client.GetStringAsync(new Uri("https://play.dhis2.org/2.29/api/organisationUnits")).Result;
+            Console.Write(response);
+        }
+    }
+}
